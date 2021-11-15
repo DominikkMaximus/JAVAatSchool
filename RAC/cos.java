452fd5c7bdbfef65523721798581138cd18c87e7
Dominik Krašovec <54041015+mgreen11m@users.noreply.github.com>
2021-11-15 11:36:00 +0100

    Create cos.java

---
+++ RAC/cos.java
@@ -0,0 +1,20 @@
+public class cos {
+
+	public static void main(String[] args) {
+		double m, f, cos,
+		x = Math.PI / 3,
+		fakulteta = 1;
+		cos = 0;
+		for (int n = 0; n<6; n++) {
+			m = 2 * n;
+			for (int k = 1; k <= m; k++) {
+				fakulteta *= k;
+			}
+			cos += (Math.pow(-1, n) / fakulteta) * Math.pow(x, m);
+		}
+		System.out.println(cos);
+
+
+	}
+
+}
