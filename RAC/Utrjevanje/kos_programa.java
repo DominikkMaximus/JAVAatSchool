fc4b5965c97440e27e07246b3fe8fad43442e301
Dominik Krašovec <54041015+mgreen11m@users.noreply.github.com>
2021-11-22 21:00:46 +0100

    Create kos_programa.java

---
+++ RAC/Utrjevanje/kos_programa.java
@@ -0,0 +1,12 @@
+public class kos_programa {
+    public static void main(String[] args) {
+        double x = 2;
+        double y = 3;
+        if (x < x - y)
+            x *= 2;
+        else
+            y += 7;
+        x++;
+        System.out.println("x= " + x + " y= " + y);
+    }
+}
