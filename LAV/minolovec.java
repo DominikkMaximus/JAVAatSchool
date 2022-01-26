500c00a3e8d506c26beb19df63de75cfa37b3c29
Dominik Krašovec <54041015+mgreen11m@users.noreply.github.com>
2022-01-26 11:44:03 +0100

    Update minolovec.java

--- LAV/minolovec.java
+++ LAV/minolovec.java
@@ -1,7 +1,8 @@
 import java.util.Random;
 
-class minolovec {
+class Main {
   public static void main(String[] args) {
+    int stevec9;
     int vrstic = 20, stoplcev = 20;
     int[][] m = new int[vrstic][stoplcev];
     izpis(m);
@@ -15,12 +16,53 @@ class minolovec {
       m[vrstica][stolpec] = 9;
     }
     izpis(m);
+
+    // preštej in oštevilči prazne celice s številom bomb v soseščini
+    int a, b, c, d;
+    for (int i = 0; i < m.length; i++) {
+      for (int j = 0; j < m[i].length; j++) {
+        if (m[i][j] == 0) {
+          a = i - 1;
+          b = i + 1;
+          c = j - 1;
+          d = j + 1;
+          if (i == 0) {
+            a = i;
+            b = j + 1;
+          }
+          if (i == m.length - 1) {
+            a = i - 1;
+            b = i;
+          }
+          if (j == 0) {
+            c = j;
+            d = j + 1;
+          }
+          if (j == m[i].length - 1) {
+            c = j - 1;
+            d = j;
+          }
+
+          for (int k = a; k <= b; k++) {
+            for (int l = c; l <= d; j++) {
+              if (m[k][l] == 9) {
+                stevec9++;
+              }
+            }
+          }
+        }
+        m[i][j] = stevec9;
+        stevec9 = 0;
+      }
+    }
+
+    izpis(m);
   }
 
   public static void izpis(int tab[][]) {
     for (int i = 0; i < tab.length; i++) {
       for (int j = 0; j < tab[i].length; j++)
-        System.out.print(tab[i][j]);
+        System.out.print(tab[i][j] + " ");
       System.out.println();
     }
     System.out.println();
