f6030d17fd57728603061ac92325678296f18d5d
Dominik Krašovec <54041015+mgreen11m@users.noreply.github.com>
2022-01-28 17:19:36 +0100

    Update introsort.java

--- RAC/introsort/introsort.java
+++ RAC/introsort/introsort.java
@@ -1,11 +1,7 @@
 public class introsort {
 
     public static void main(String[] args) {
-        int[] tabela = { 145, 162, 159, 194, 51, 64, 160, 8, 16, 18, 126, 148, 74, 181, 164, 58, 68, 84, 43, 9, 103,
-                161, 19, 146, 38, 121, 182, 49, 188, 44, 92, 71, 79, 198, 163, 57, 186, 97, 118, 48, 61, 46, 114, 37,
-                40, 45, 119, 32, 106, 2, 55, 41, 78, 85, 10, 117, 34, 179, 109, 183, 54, 200, 80, 175, 59, 156, 133,
-                137, 36, 6, 176, 25, 174, 138, 62, 77, 115, 141, 108, 170, 169, 191, 88, 130, 107, 28, 112, 165, 87, 73,
-                149, 76, 69, 20, 105, 7, 147, 197, 135, 116 };
+        int[] tabela = { 145, 162, 159, 194, 51, 64, 160, 8, 16, 18, 126, 148 };
         introSort(tabela, tabela.length - 1);
         izpis(tabela);
     }
@@ -28,7 +24,6 @@ public class introsort {
         // preuredi da bo "max heap"
         for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(tab, n, i);
-            System.out.println("heapify " + i);
         }
         // sortiraj
         for (int i = n - 1; i > 0; i--) {
@@ -70,9 +65,6 @@ public class introsort {
             introSort(tab, i - 1);
         else if (neurejeni_konec - i < 16) {
             insertionSort(tab, 0, neurejeni_konec + 2);
-            System.out.println("po zadnjem insertion sortu: ");
-            izpis(tab);
-            System.out.println();
         } else {
             //naredi novo tabelo delTabele
             int delTabele[] = new int[neurejeni_konec - i + 2];
@@ -83,8 +75,6 @@ public class introsort {
             for(int k=0;k<delTabele.length;k++){
                 tab[i+k]=delTabele[k];
             }
-            System.out.println("po zadnjem heap sortu: ");
-            izpis(tab);
         }
     }
 
@@ -95,8 +85,6 @@ public class introsort {
         tab[neurejeni_konec / 2] = tmp[1];
         tab[neurejeni_konec] = tmp[2];
         menjaj(tab, neurejeni_konec / 2, neurejeni_konec - 1);
-        izpis(tab);
-        System.out.println();
         int pivot = tab[neurejeni_konec - 1];
         int i = 0;
         int j = neurejeni_konec - 2;// začni enomesto levo od pivota
@@ -107,22 +95,13 @@ public class introsort {
                 j--;
             if (i <= j) {// dokler se i in j ne križata
                 menjaj(tab, i, j);
-                System.out.println("i: " + i + " j: " + j);
-                izpis(tab);
                 i++;
                 j--;
             }
         }
         menjaj(tab, i, neurejeni_konec - 1);// ko se i in j križata, je pivot na mestu
-        System.out.println("i: " + i + " j: " + j);
-        System.out.println("po intro sortu: ");
-        izpis(tab);
         if (neurejeni_konec - i < 16) {
             insertionSort(tab, i, neurejeni_konec + 1);
-            System.out.println("po insertion sortu: ");
-            izpis(tab);
-            System.out.println();
-
         } else {
             int delTabele[] = new int[neurejeni_konec - i + 1];
             for (int k = 0; k < delTabele.length; k++) {
@@ -132,14 +111,7 @@ public class introsort {
             for(int k=0;k<delTabele.length;k++){
                 tab[i+k]=delTabele[k];
             }
-            //formiraj novo tabelo delTabele
-            System.out.println("po heap sortu: ");
-            izpis(tab);
-            System.out.println();
         }
-        System.out.println("pred ponovnim introsortom i: " + i + " j: " + j + " neurejeni konec: " + neurejeni_konec);
         leviDel(tab, i, neurejeni_konec);
-
     }
-
 }
