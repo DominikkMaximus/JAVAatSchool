1de3aaf9ab7f3c08dec5f60301afdde2fdbfee4f
Dominik Krašovec <54041015+mgreen11m@users.noreply.github.com>
2022-02-02 12:02:07 +0100

    Create sumdiagonal.java

---
+++ LAV/preverjanje2/sumdiagonal.java
@@ -0,0 +1,11 @@
+class sumdiagonal {
+  public static void main(String[] args) {
+    int tab[][] = new int[9][9];
+    //napolni tabelo, drugace sta vsoti 0
+    int diagonal1 = 0;
+    int diagonal2 = 0;
+    for (int i = 0; i < tab.length; i++){
+      diagonal1+=tab[i][i];
+      diagonal2+=tab[i][tab.length-1-i];
+    }
+    S
