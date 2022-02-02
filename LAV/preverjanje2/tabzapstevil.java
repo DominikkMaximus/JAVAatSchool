cd2b69f50fbc4f6753b1ed1c1083423f38c327c4
Dominik Krašovec <54041015+mgreen11m@users.noreply.github.com>
2022-02-02 11:54:48 +0100

    Update tabzapstevil.java

--- LAV/preverjanje2/tabzapstevil.java
+++ LAV/preverjanje2/tabzapstevil.java
@@ -3,12 +3,9 @@ class tabzapstevil {
   public static void main(String[] args) {
     int r[][] = new int[6][7];
     int stevilo=1;
-    for(int i=0;i<r.length;i++){
-      for( int j=0;j<r[i].length;j++){
-        r[i][j]= stevilo;
-        stevilo++;
-      }
-    }
+    for(int i=0;i<r.length;i++)
+      for( int j=0;j<r[i].length;j++)
+        r[i][j]= stevilo++;
     izpis2d(r);
   }
    static void izpis2d(int tab[][]){
@@ -16,7 +13,7 @@ class tabzapstevil {
       for( int j=0;j<tab[i].length;j++){
         System.out.print(tab[i][j]+" ");
       }
-      System.out.println();}}
-    
-
+      System.out.println();
+    }
 }
+  
