4b6971a011cf89c577c0190a03d728391ce15b27
Dominik Krašovec <54041015+mgreen11m@users.noreply.github.com>
2022-02-23 11:22:08 +0100

    Create izpisvsvojovrstico.java

---
+++ LAV/string/izpisvsvojovrstico.java
@@ -0,0 +1,6 @@
+public static void izpisvsvojovrstico(String[] args) {
+    String besedilo="gaiwrghj 3493tnj 42t53 iojrbug";
+    for(int i=0;i<besedilo.length();i++){
+      System.out.println(besedilo.charAt(i));
+    }
+  }
