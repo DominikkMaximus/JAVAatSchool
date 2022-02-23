eb487fab3d0771a1a7bcd4692053c2ac5bbdc905
Dominik Krašovec <54041015+mgreen11m@users.noreply.github.com>
2022-02-23 11:40:18 +0100

    Create jepalindrom.java

---
+++ LAV/string/jepalindrom.java
@@ -0,0 +1,24 @@
+  public static void jepalindrom(String[] args) {
+    String niz="pericarezeracirep";
+    //brez zanke
+    StringBuffer reversed= new StringBuffer(niz);
+    reversed.reverse();
+    if(reversed.toString().compareTo(niz)==0){
+      System.out.println("Je palindrom");
+    }
+    else
+      System.out.println("Ni palindrom");
+  
+  //z zanko
+  boolean jepalindrom=true;
+  for(int i=0;i<niz.length();i++){
+    if(niz.charAt(i)!=niz.charAt(niz.length()-1-i)){
+      jepalindrom=false;
+      break;
+    }
+  }
+  if(jepalindrom)
+    System.out.println("Je palindrom");
+  else
+    System.out.println("Ni palindrom"); 
+  }
