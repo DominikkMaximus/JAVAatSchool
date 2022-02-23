937825a68264140bcb2216f26fbf772571a5b462
Dominik Krašovec <54041015+mgreen11m@users.noreply.github.com>
2022-02-23 11:16:33 +0100

    Create stejsubstring.java

---
+++ LAV/string/stejsubstring.java
@@ -0,0 +1,19 @@
+class stejsubstring {
+  public static void main(String[] args) {
+    String besedilo ="danes je lep dan";
+    String substring = "dan";
+    int stPonvitev =stejPonovitve(besedilo,substring); 
+    System.out.println(stPonvitev);
+  }
+  public static int stejPonovitve(String niz, String substring){
+    int stPonovitev=0;
+    int index=0;
+    while((index=niz.indexOf(substring,index))!=-1){
+  
+      stPonovitev++;
+      index+=substring.length();
+      System.out.println(stPonovitev);
+    }
+    return stPonovitev;
+  }
+}
