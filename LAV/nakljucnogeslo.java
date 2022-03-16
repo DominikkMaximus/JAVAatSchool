521fecc24c7c1dace7a4600d61e4ddf084e6e9ec
Dominik Krašovec <54041015+mgreen11m@users.noreply.github.com>
2022-03-16 11:02:10 +0100

    Update nakljucnogeslo.java

--- LAV/nakljucnogeslo.java
+++ LAV/nakljucnogeslo.java
@@ -4,7 +4,7 @@ class nakljucnogeslo {
     String mozniZnaki ="ABCDEFGHIJKLMNOPRSTUVZQWYXabcdefghijklmnoprstuvzqwyx0123456789", geslo="";
     Random r=new Random();
     for(int i=0;i<8;i++)
-      geslo=geslo.concat(""+mozniZnaki.charAt(r.nextInt(mozniZnaki.length()-1)));
+      geslo=geslo.concat(""+mozniZnaki.charAt(r.nextInt(mozniZnaki.length())));
     System.out.println(geslo);
   }
 }
