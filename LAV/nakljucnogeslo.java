85fba46781727aceb55585c126ff502e03e7276d
Dominik Krašovec <54041015+mgreen11m@users.noreply.github.com>
2022-03-16 10:59:55 +0100

    Create nakljucnogeslo.java

---
+++ LAV/nakljucnogeslo.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+class nakljucnogeslo {
+  public static void main(String[] args) {
+    String mozniZnaki ="ABCDEFGHIJKLMNOPRSTUVZQWYXabcdefghijklmnoprstuvzqwyx0123456789", geslo="";
+    Random r=new Random();
+    for(int i=0;i<8;i++)
+      geslo=geslo.concat(""+mozniZnaki.charAt(r.nextInt(mozniZnaki.length()-1)));
+    System.out.println(geslo);
+  }
+}
